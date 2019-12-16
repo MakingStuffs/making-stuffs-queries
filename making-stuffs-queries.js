c31a8f308338244ddef832db008624ed44285401
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-16 15:45:06 +0000

    Created msCreate as a function

--- making-stuffs-queries.js
+++ making-stuffs-queries.js
@@ -6,3 +6,15 @@ export const msQuery = (selector = 'body', elem = document) => {
 export const msQueryAll = (selector = 'a', elem = document) => {
     return elem.querySelectorAll(selector);
 }
+
+export const msCreate = (elem = null, params = null) => {
+    const elemType = !elem ? 'div' : elem;
+    const newElem = document.createElement(elemType);
+    if(params && typeof params === 'object') {
+        const attributes = Object.keys(params);
+        for(let i = 0; i < attributes.length; i++) {
+            newElem.setAttribute(attributes[i], params[attributes[i]]);
+        }
+    }
+    return newElem;
+}
