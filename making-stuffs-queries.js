1ab37f8c67645d9c661c354a703257a4ef0de2dc
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-16 14:27:03 +0000

    Initial commit

---
+++ making-stuffs-queries.js
@@ -0,0 +1,8 @@
+"use strict";
+export const msQuery = (selector = 'body', elem = document) => {
+    return elem.querySelector(selector);
+}
+
+export const msQueryAll = (selector = 'a', elem = document) => {
+    return elem.querySelectorAll(selector);
+}
