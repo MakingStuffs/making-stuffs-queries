2460c95cf1c5fbc746d329628baf52aa72d9e524
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2019-12-18 00:48:57 +0000

    Created msAppend function which will append children provided to either a provided parent of the default of the document.body

--- making-stuffs-queries.js
+++ making-stuffs-queries.js
@@ -10,11 +10,26 @@ export const msQueryAll = (selector = 'a', elem = document) => {
 export const msCreate = (elem = null, params = null) => {
     const elemType = !elem || typeof elem !== 'string' ? 'div' : elem;
     const newElem = document.createElement(elemType);
-    if(typeof params === 'object' && !!params && !Array.isArray(params)) {
+    if (typeof params === 'object' && !!params && !Array.isArray(params)) {
         const attributes = Object.keys(params);
-        for(let i = 0; i < attributes.length; i++) {
+        for (let i = 0; i < attributes.length; i++) {
             newElem.setAttribute(attributes[i], params[attributes[i]]);
         }
     }
     return newElem;
+}
+
+export const msAppend = (children = null, parent = document.body) => {
+    if (!children || !children instanceof HTMLElement || !Array.isArray(children)) return;
+    if (Array.isArray(children)) {
+        for(let child of children) {
+            if(child instanceof HTMLElement) {
+                parent.append(child);
+            } else {
+                return;
+            }
+        }
+    } else {
+        return parent.append(children);
+    }
 }
